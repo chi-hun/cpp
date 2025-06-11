63530e36fd99e98bc6d20f960e194fe9bc137db2
chi-hun <huunnii@gmail.com>
2025-06-11 08:39:42 +0000

    prime

--- hello.c
+++ hello.c
@@ -1,16 +1,29 @@
 // 7 while
 #include <stdio.h>
+#include <math.h>
 
 int main() {
-    int n;
-    char ss = ' ';
-    char st = '*';
-    scanf("%d", &n);
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= i;  j++) {
-            printf("%c", st);
+    int ct = 2;
+    int prime[1000];
+    prime[0] = 2;
+    prime[1] = 3;
+    for (int i = 4; i < 1000000; i++) {
+        printf("%d \n", i);
+        int flag = 0;
+        for (int j = 0; j < ct; j++) {
+            if (i % prime[j] == 0) {
+                flag = 1;
+                break;
+            }
+        if (flag == 0) {
+            prime[ct] = i;
+            ct++;
+            // printf("%d \n", i);
+            if (ct == 1001) {
+                break;
+            }
         }
-        printf("\n");
+        }
     }
     return 0;
 }
