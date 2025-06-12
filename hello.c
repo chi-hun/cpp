28d747e804f40710c4df19935fcf2fc202b89c99
chi-hun <huunnii@gmail.com>
2025-06-12 08:47:40 +0000

    point

--- hello.c
+++ hello.c
@@ -1,29 +1,16 @@
 // 11-1 
 #include <stdio.h>
-#include <math.h>
-
 int main() {
-    int ct = 2;
-    int prime[1000];
-    prime[0] = 2;
-    prime[1] = 3;
-    for (int i = 4; i < 1000000; i++) {
-        // printf("%d \n", i);
-        int flag = 0;
-        for (int j = 0; j < ct; j++) {
-            if (i % prime[j] == 0) {
-                flag = 1;
-                break;
-            }
-        }
-        if (flag == 0) {
-            prime[ct] = i;
-            ct++;
-            printf("%d \n", i);
-            if (ct == 1001) {
-                break;
-            }
-        }
-    }
-    return 0;
+  int a = 3;
+  int b = 4;
+  int c = 5;
+  int *parr[3];
+  parr[0] = &a;
+  parr[1] = &b;
+  parr[2] = &c;
+  printf("%d \n", *(parr[0]+1));
+  printf("%d \n", *(parr[1]));
+
+
+  return 0;
 }
