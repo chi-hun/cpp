581496ce38359d95b358ef100b1679026c0729fc
chi-hun <huunnii@gmail.com>
2025-06-13 07:32:36 +0000

    prime_2

--- hello.c
+++ hello.c
@@ -1,16 +1,36 @@
 // 11-1 
 #include <stdio.h>
-int main() {
-  int a = 3;
-  int b = 4;
-  int c = 5;
-  int *parr[3];
-  parr[0] = &a;
-  parr[1] = &b;
-  parr[2] = &c;
-  printf("%d \n", *(parr[0]+1));
-  printf("%d \n", *(parr[1]));
 
+void is_prime(int *arr);
+int main() {
+  int prime_list[1000];
+
+  for (int i = 0; i < 1000; i++) {
+    prime_list[i] = 1;
+  }
+
+  prime_list[0] = 0;
+  prime_list[1] = 0;
+
+
+  is_prime(prime_list);
+
+  for (int i = 2; i < 1000; i++) {
+    if (prime_list[i] == 1) {
+      printf("%d\n", i);
+    }
+  }
 
   return 0;
 }
+
+void is_prime(int *arr) {
+  for (int i = 2; i * i < 1000; i++) {
+    if (arr[i] == 1) {
+      for (int j = i * i; j < 1000; j += i) {
+        arr[j] = 0;
+      }
+    }
+  }
+}
+
