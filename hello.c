0db7a2c179c4cd5a76aed32005a2ee588c7e2a80
chi-hun <huunnii@gmail.com>
2025-06-13 08:21:06 +0000

    string

--- hello.c
+++ hello.c
@@ -1,36 +1,18 @@
-// 11-1 
+// 15-2 
 #include <stdio.h>
 
 void is_prime(int *arr);
 int main() {
-  int prime_list[1000];
-
-  for (int i = 0; i < 1000; i++) {
-    prime_list[i] = 1;
+  char st[4];
+  if (fgets(st, sizeof(st), stdin) != NULL) {
+   printf("%s 맞나요?", st);
+  } else{
+    printf("xx");
   }
 
-  prime_list[0] = 0;
-  prime_list[1] = 0;
+  char sts[4] = "ddd";
+  printf("%s", sts);
 
-
-  is_prime(prime_list);
-
-  for (int i = 2; i < 1000; i++) {
-    if (prime_list[i] == 1) {
-      printf("%d\n", i);
-    }
-  }
-
+  
   return 0;
 }
-
-void is_prime(int *arr) {
-  for (int i = 2; i * i < 1000; i++) {
-    if (arr[i] == 1) {
-      for (int j = i * i; j < 1000; j += i) {
-        arr[j] = 0;
-      }
-    }
-  }
-}
-
