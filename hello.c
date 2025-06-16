48096210a166b7d3827aef5458930e3af6b87b8f
chi-hun <huunnii@gmail.com>
2025-06-16 08:43:18 +0000

    struct

--- hello.c
+++ hello.c
@@ -1,18 +1,29 @@
 // 15-2 
 #include <stdio.h>
+#include <string.h>
 
-void is_prime(int *arr);
+struct Human {
+  char name[20];
+  int age;
+  int height;
+  int weight;
+};
+
+int input_human(struct Human *h, const char *name, int age, int height, int weight);
+
 int main() {
-  char st[4];
-  if (fgets(st, sizeof(st), stdin) != NULL) {
-   printf("%s 맞나요?", st);
-  } else{
-    printf("xx");
-  }
+  struct Human man_1;
+  struct Human *man_1_p = &man_1;
+  input_human(man_1_p, "kim", 40, 175, 80);
+  printf("%s %d %d %d\n", man_1.name, man_1.age, man_1.height, man_1.weight);
 
-  char sts[4] = "ddd";
-  printf("%s", sts);
+  return 0;
+}
 
-  
+int input_human(struct Human *h, const char *name, int age, int height, int weight) {
+  strcpy(h->name, name);
+  h->age = age;
+  h->height = height;
+  h->weight = weight;
   return 0;
 }
