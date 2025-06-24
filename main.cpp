0fac5889f576cb3cc6f3b2d68cf38ca4866d0e04
chi-hun <huunnii@gmail.com>
2025-06-24 08:39:38 +0000

    marine_2

--- main.cpp
+++ main.cpp
@@ -1,60 +1,98 @@
-// 1-3
+// 4-4
 #include <iostream>
 #include <string.h>
-#include <cmath>
 
-class Ball {
+class Marine {
     private:
+        static int total_num;
+        char* name;
+        int hp;
+        int damage;
         int x;
         int y;
-    public:
-        Ball (int _x, int _y) {
-            x = _x;
-            y = _y;
-        }
+        bool is_dead;
 
-        void print();
+    public:
+        Marine();
+        Marine(int x, int y);
+        Marine(const Marine& master);
+        ~Marine();
+        void show_status();
         void move(int _x, int _y);
-        int s_x();
-        int s_y();
-        float distance(Ball ball_d);
+        int attack();
+        void be_attacked(int damage);
+        int& access_damage();
 };
 
-void Ball::print() {
-    std::cout << "x : " << x << ", " << "y : " << y << std::endl;
+int Marine::total_num = 0;
+
+Marine::Marine() : name(NULL), hp(50), damage(5), x(0), y(0), is_dead(false) {
+    total_num++;
 }
 
-void Ball::move(int _x, int _y) {
-    x += _x;
-    y += _y;
+Marine::Marine (int x, int y) : name(NULL), hp(50), damage(5), x(x), y(y), is_dead(false) {
+    total_num++;
 }
 
-int Ball::s_x() {
-    return x;
+Marine::Marine(const Marine& master) {
+    name = new char[strlen(master.name) + 1];
+    strcpy(name, master.name);
+    hp = master.hp;
+    damage = master.damage;
+    x = master.x;
+    y = master.y;
+    is_dead = false;
 }
 
-int Ball::s_y() {
-    return y;
+Marine::~Marine() {
+    std::cout << "delete " << name << std::endl;
+    total_num--;
+    delete[] name;
 }
 
-float Ball::distance(Ball ball_d) {
-    int x_d = ball_d.s_x();
-    int y_d = ball_d.s_y();
-    
-    return std::sqrt((x - x_d) * (x - x_d) + (y - y_d) * (y - y_d));
-}   
-    
+void Marine::show_status() {
+    if (name == NULL) {
+        std::cout << "no name" << std::endl;
+    } else {
+        std::cout << "name: " << name << std::endl;
+    }
+    std::cout << "hp: " << hp << std::endl;
+    std::cout << "damage: " << damage << std::endl;
+    std::cout << "position: (" << x << ", " << y << ")" << std::endl;
+    std::cout << "is_dead: " << (is_dead ? "true" : "false") << std::endl;
+    std::cout << "total_num: " << total_num << std::endl;
+}
+
+void Marine::move(int _x, int _y) {
+    x = _x;
+    y = _y;
+}
+
+int Marine::attack() {
+    return damage;
+}
+
+void Marine::be_attacked(int damage) {
+    hp -= damage;
+    if (hp <= 0) {
+        is_dead = true;
+    }
+}
+
+int& Marine::access_damage() {
+    return damage;
+}
+
 int main(void) {
-    Ball ball_1(1,2);
-    ball_1.print();
+    Marine marine_1;
+    marine_1.show_status();
 
-    Ball ball_2(3,4);
-    ball_2.print();
-    ball_2.move(1,1);
-    ball_2.print();
+    Marine marine_2(3, 4);
+    marine_2.show_status();
 
-    int dd = ball_1.distance(ball_2);
-    std::cout << dd << std::endl;
+    int& marine_2_access_damage = marine_2.access_damage();
+    marine_2_access_damage = 50;
+    marine_2.show_status();
 
     return 0;
 }
