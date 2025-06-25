da059417e75a88ce4cc2d22e1eab6ee142b09680
chi-hun <huunnii@gmail.com>
2025-06-25 03:08:42 +0000

    my_string

--- main.cpp
+++ main.cpp
@@ -2,97 +2,136 @@
 #include <iostream>
 #include <string.h>
 
-class Marine {
-    private:
-        static int total_num;
-        char* name;
-        int hp;
-        int damage;
-        int x;
-        int y;
-        bool is_dead;
+class Mystring {
+    char* str_p;
+    int str_l;
+    int str_c;
 
     public:
-        Marine();
-        Marine(int x, int y);
-        Marine(const Marine& master);
-        ~Marine();
-        void show_status();
-        void move(int _x, int _y);
-        int attack();
-        void be_attacked(int damage);
-        int& access_damage();
+    Mystring(char c);
+    Mystring(const char *c);
+    Mystring(const Mystring& c);
+    ~Mystring();
+    Mystring& assign(const Mystring& c);
+    Mystring& assign(const char *c);
+    Mystring& operator=(const Mystring& c);
+    Mystring operator+(const Mystring& c);
+    int length() const;
+    int cap() const;
+    void re_cap(int size);
+    void print();
 };
 
-int Marine::total_num = 0;
-
-Marine::Marine() : name(NULL), hp(50), damage(5), x(0), y(0), is_dead(false) {
-    total_num++;
+Mystring::Mystring(char c) {
+    str_p = new char[1];
+    str_p[0] = c;
+    str_l = 1;
+    str_c = 1;
 }
 
-Marine::Marine (int x, int y) : name(NULL), hp(50), damage(5), x(x), y(y), is_dead(false) {
-    total_num++;
+Mystring::Mystring(const char *c) {
+    str_l = strlen(c);
+    str_c = str_l+1;
+    str_p = new char[str_l+1];
+    strcpy(str_p, c);
 }
 
-Marine::Marine(const Marine& master) {
-    name = new char[strlen(master.name) + 1];
-    strcpy(name, master.name);
-    hp = master.hp;
-    damage = master.damage;
-    x = master.x;
-    y = master.y;
-    is_dead = false;
+Mystring::Mystring(const Mystring& c) {
+    str_l = strlen(c.str_p);
+    str_c = str_l+1;
+    str_p = new char[str_l+1];
+    strcpy(str_p, c.str_p);
 }
 
-Marine::~Marine() {
-    std::cout << "delete " << name << std::endl;
-    total_num--;
-    delete[] name;
+Mystring::~Mystring() {
+    delete[] str_p;
 }
 
-void Marine::show_status() {
-    if (name == NULL) {
-        std::cout << "no name" << std::endl;
-    } else {
-        std::cout << "name: " << name << std::endl;
+Mystring& Mystring::assign(const Mystring& c) {
+    if (str_l < c.str_l) {
+        delete[] str_p;
+        str_p = new char[c.str_l+1];
+        str_c = c.str_l+1;
     }
-    std::cout << "hp: " << hp << std::endl;
-    std::cout << "damage: " << damage << std::endl;
-    std::cout << "position: (" << x << ", " << y << ")" << std::endl;
-    std::cout << "is_dead: " << (is_dead ? "true" : "false") << std::endl;
-    std::cout << "total_num: " << total_num << std::endl;
+    strcpy(str_p, c.str_p);
+    str_l = c.str_l;
+    return *this;
 }
 
-void Marine::move(int _x, int _y) {
-    x = _x;
-    y = _y;
+Mystring& Mystring::assign(const char *c) {
+    int c_l = strlen(c);
+    if (str_l < c_l) {
+        delete[] str_p;
+        str_p = new char[c_l+1];
+    }
+    strcpy(str_p, c);
+    str_l = c_l;
+    return *this;
+}
+Mystring& Mystring::operator=(const Mystring& c) {
+    if (str_l < c.str_l) {
+        delete[] str_p;
+        str_p = new char[c.str_l+1];
+    }
+    strcpy(str_p, c.str_p);
+    str_l = c.str_l;
+    return *this;
 }
 
-int Marine::attack() {
-    return damage;
+Mystring Mystring::operator+(const Mystring& c) {
+    Mystring tmp('e');
+    tmp.str_l = str_l + c.str_l;
+    tmp.str_c = tmp.str_l + 1;
+    tmp.str_p = new char[tmp.str_c];
+    strcpy(tmp.str_p, str_p);
+    strcat(tmp.str_p, c.str_p);
+    return tmp;
 }
 
-void Marine::be_attacked(int damage) {
-    hp -= damage;
-    if (hp <= 0) {
-        is_dead = true;
+int Mystring::length() const {
+    return str_l;
+}
+
+int Mystring::cap() const {
+    return str_c;
+}
+
+void Mystring::re_cap(int size) {
+    if (str_c < size) {
+        char* tmp = str_p;
+        str_p = new char[size];
+        strcpy(str_p, tmp);
+        str_c = size;
+        delete[] tmp;
     }
 }
 
-int& Marine::access_damage() {
-    return damage;
+
+void Mystring::print() {
+    std::cout << str_p << std::endl;
 }
 
 int main(void) {
-    Marine marine_1;
-    marine_1.show_status();
+    Mystring cc("ddd");
+    cc.print();
+    Mystring dd(cc);
+    dd.print();
+    std::cout << dd.length() << std::endl;
 
-    Marine marine_2(3, 4);
-    marine_2.show_status();
+    dd.assign("abc");
+    dd.print();
+    std::cout << dd.length() << std::endl;
 
-    int& marine_2_access_damage = marine_2.access_damage();
-    marine_2_access_damage = 50;
-    marine_2.show_status();
+    dd = cc;
+    dd.print();
+    std::cout << dd.length() << std::endl;
+    std::cout << dd.cap() << std::endl;
+
+    dd.re_cap(100);
+    std::cout << dd.cap() << std::endl;
+
+    Mystring aa = cc + dd;
+    aa.print();
 
     return 0;
 }
