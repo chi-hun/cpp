4bc0d321e5a3b2184e814d197c6dc033d1d8633b
chi-hun <huunnii@gmail.com>
2025-06-27 07:48:41 +0000

    c_slave

--- main.cpp
+++ main.cpp
@@ -1,144 +1,128 @@
 // 5-4
 #include <iostream>
+#include <string>
 
-namespace MyArray {
-class Array;
-class Int;
+class C_slave {
+    // private:
+    //     std::string name;
+    //     std::string position;
+    //     int age;
+    //     int grade;
 
-class Array {
-    private:
-        const int dim;
-        int* size;
-        friend class Int;
+    protected:
+        std::string name;
+        std::string position;
+        int age;
+        int grade;
 
-        struct Address {
-            int level;
-            void* next;
-        };
-        Address* top;
-    
     public:
-        Array(int dim, int* array_size) : dim(dim) {
-            size = new int[dim];
-            for (int i = 0; i < dim; i++) {
-                size[i] = array_size[i];
-
-            top = new Address;
-            top -> level = 0;
-            initial_address(top);
-            }
-        }
-        Array(const Array& arr) : dim(arr.dim) {
-            size = new int[dim];
-            for (int i = 0; i < dim; i++) {
-                size[i] = arr.size[i];
-            }
-            top = new Address;
-            top -> level = 0;
-            initial_address(top);
-            copy_address(top, arr.top);
-        }
-        ~Array() {
-            delete_address(top);
-            delete[] size;
-        }
-        void initial_address(Address* current);
-        void delete_address(Address* current);
-        void copy_address(Address* to, Address* from);
-        Int operator[](const int index);
-
+        C_slave(){};
+        C_slave(std::string name, std::string position, int age, int grade) 
+        : name(name), position(position), age(age), grade(grade) {};
+        C_slave(C_slave& slave) : name(slave.name), position(slave.position), age(slave.age), grade(slave.grade) {};
+        ~C_slave(){};
+        int calculate_pay();
+        void print_info();
 };
 
-class Int {
-        private:
-            int level;
-            void* data;
-            Array* arr;
-
-        public:
-            Int(int index, int level =0, void* data = nullptr, Array* arr = nullptr) 
-            : level(level), data(data), arr(arr) {
-                if (level < 1 || index >= arr -> size[level - 1]) {
-                    data = nullptr;
-                    return;
-                }
-                if (level == arr -> dim) {
-                    data = static_cast<void*>(
-                        static_cast<int*>(static_cast<Array::Address*>(data) -> next) + index);
-                } else {
-                    data = static_cast<void*>(static_cast<Array::Address*>(static_cast<Array::Address*>(data) -> next) + index);
-                }
-            }
-        Int operator[](int index) {
-            return Int(index, level + 1, data, arr);
-        }
-
-        operator int() {
-            if (data) return *static_cast<int*>(data);
-            return 0;
-        }
+class C_slave_h : public C_slave {
+    private:
+        int work_year;
+    
+    public:
+        C_slave_h() {};
+        C_slave_h(std::string name, std::string position, int age, int grade, int work_year) 
+        : C_slave(name, position, age, grade), work_year(work_year) {}; 
+        C_slave_h(C_slave_h& slave) 
+        : C_slave(slave.name, slave.position, slave.age, slave.grade), work_year(slave.work_year) {};
+        ~C_slave_h(){};
+        int calculate_pay();
+        void print_info();
         
 };
 
-void Array::initial_address(Address* current) {
-    if (!current) {return;}
-    if (current -> level == dim - 1) {
-        current -> next = new int[size[current -> level]];
-        return;
-    }
-    current -> next = new Address[size[current -> level]];
-    for (int i = 0; i < size[current -> level]; i++) {
-        (static_cast<Address*>(current -> next) + i) -> level = current -> level + 1;
-        initial_address(static_cast<Address*>(current -> next) + i);
-    }
+class C_slave_list {
+    private:
+        int alloc_slave;
+        int current_slave;
+        int current_slave_h;
+        C_slave** slave_list;
+        C_slave_h** slave_list_h;
+
+    public:
+        C_slave_list(int alloc_slave) : alloc_slave(alloc_slave), current_slave(0), current_slave_h(0) {
+            slave_list = new C_slave*[alloc_slave];
+            slave_list_h = new C_slave_h*[alloc_slave];
+        };
+        ~C_slave_list();
+        void add_slave(C_slave* slave);
+        void add_slave_h(C_slave_h* slave);
+        int slave_num() {return current_slave + current_slave_h;};
+        void print_info();
+
+};
+
+int C_slave::calculate_pay() {
+    int pay = 200 + grade *50;
+    return pay;
 }
 
-void Array::delete_address(Address* current) {
-    if (!current) {return;}
-    for (int i = 0; current->level != dim - 1 && i < size[current -> level]; i++) {
-        delete_address(static_cast<Address*>(current -> next) + i);        
-    }
-    if (current -> level == dim - 1) {
-        delete[] static_cast<int*>(current -> next);
-    }else{
-        delete[] static_cast<Address*>(current -> next);
-    }
+void C_slave::print_info() {
+    std::cout << "이름 : " << name << std::endl;
+    std::cout << "직위 : " << position << std::endl;
+    std::cout << "나이 : " << age << std::endl;
+    std::cout << "급여 : " << calculate_pay() << std::endl;
 }
 
-void Array::copy_address(Address* to, Address* from) {
-    if (to -> level == dim - 1) {
-        for (int i = 0; i < size[to -> level]; i++) {
-            static_cast<int*>(to -> next)[i] = static_cast<int*>(from -> next)[i];
-        }
-        return;
-    }
-    for (int i = 0; i < size[to -> level]; i++) {
-        Address* to_next = static_cast<Address*>(to -> next) + 1;
-        Address* from_next = static_cast<Address*>(from -> next) + 1;
-        copy_address(to_next, from_next);
-    }
+int C_slave_h::calculate_pay() {
+    int pay = 200 + grade *50 + work_year * 10;
+    return pay;
 }
 
-Int Array::operator[](const int index) {
-        return Int(index, 1, static_cast<void*>(top), this);
-        }
-
+void C_slave_h::print_info() {
+    std::cout << "이름 : " << name << std::endl;
+    std::cout << "직위 : " << position << std::endl;
+    std::cout << "나이 : " << age << std::endl;
+    std::cout << "근속년수 : " << work_year << std::endl;
+    std::cout << "급여 : " << calculate_pay() << std::endl;
 }
 
+void C_slave_list::add_slave(C_slave* slave) {
+    slave_list[current_slave] = slave;
+    current_slave++;
+}
+
+void C_slave_list::add_slave_h(C_slave_h* slave) {
+    slave_list_h[current_slave_h] = slave;
+    current_slave_h++;
+}
+
+C_slave_list::~C_slave_list() {
+    for (int i = 0; i < current_slave; i++) {
+        delete slave_list[i];
+    }
+    delete[] slave_list;
+}
+void C_slave_list::print_info() {
+    int total_pay = 0;
+    for (int i = 0; i < current_slave; i++) {
+        slave_list[i] -> print_info();
+        total_pay += slave_list[i] -> calculate_pay();
+    }
+    for (int i = 0; i < current_slave_h; i++) {
+        slave_list_h[i] -> print_info();
+        total_pay += slave_list_h[i] -> calculate_pay();
+    }
+    std::cout << "총 급여 : " << total_pay << std::endl;
+}
 
 int main(void) {
-    int size[2] = {2,2};
-    MyArray::Array arr(3, size);
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 2; j++) {
-            arr[i][j] = (i+1)*(j+1);
-        }
-    }
-    // for (int i = 0; i < 2; i++) {
-    //     for (int j = 0; j < 2; j++) {
-    //         std::cout <<arr[i][j] << std::endl;
-    //     }
-    // }
-    std::cout << arr[0][1];
+    C_slave_list k_slave_list(10);
+    k_slave_list.add_slave(new C_slave("김모씨", "대리", 25, 1 ));
+    k_slave_list.add_slave(new C_slave("이모씨", "과장", 27, 2 ));
+    k_slave_list.add_slave(new C_slave("박모씨", "부장", 29, 3 ));
+    k_slave_list.add_slave_h(new C_slave_h("최모씨", "차장", 31, 4, 5));
+    k_slave_list.print_info();
+
     return 0;
 }
