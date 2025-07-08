31fade38a1556b00f96699e803cb4accc9a604d8
chi-hun <huunnii@gmail.com>
2025-07-08 01:41:29 +0000

    prime

--- main.cpp
+++ main.cpp
@@ -1,39 +1,59 @@
-// 9-3
+// 9-4
 #include <iostream>
 
 using namespace std;
 
-template <int i, int j>
-struct gcd {
-    static const int value = gcd<j, i % j>::value;
+template <int N>
+struct NUM {
+    static const int num = N;
 };
 
-template <int j>
-struct gcd<j, 0> {
-    static const int value = j;
+using one = NUM<1>;
+using two = NUM<2>;
+using three = NUM<3>;
+
+template <typename A, typename B>
+struct add{
+    using result = NUM<A::num + B::num>;
 };
 
-template <int N, int D>
-struct ratio {
-    static const int _gcd = gcd<N, D>::value;
-    using type = ratio<N / _gcd, D / _gcd>;
-    static const int num = N / _gcd;
-    static const int den = D / _gcd;
+template <typename A, typename B>
+struct divine{
+    using result = NUM<A::num / B::num>;
 };
 
-template <class A, class B>
-struct _ratio_add { 
-    using type = ratio<A::num * B::den + B::num * A::den, A::den * B::den>;
+template <typename N, typename D>
+struct check_divine {
+    static const bool result = (N::num % D::num == 0) || check_divine<N, typename add<D, one>::result>::result;
 };
 
-template <class A, class B>
-struct ratio_add : _ratio_add<A, B>::type {};
+template <typename N>
+struct _is_prime {
+    static const bool result = !check_divine<N, two>::result;
+};
 
+
+template <>
+struct _is_prime<two> {
+    static const bool result = true;
+};
+
+template <>
+struct _is_prime<three> {
+    static const bool result = true;
+};
+
+template <int N>
+struct is_prime {
+    static const bool result = _is_prime<NUM<N>>::result;
+};
+
+template <typename N>
+struct check_divine <N, typename divine<N, two>::result>{
+    static const bool result = (N::num % (N::num / 2) == 0);
+};
 
 int main() {
-    typedef ratio<3, 2> r_1;
-    typedef ratio<9, 3> r_2;
-    typedef ratio_add<r_1, r_2> r_3;
-    cout << r_3::num << "/" << r_3::den << endl;
+    cout << is_prime<41>::result;
     return 0;
 }
