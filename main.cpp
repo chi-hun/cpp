94e30b6aa3ff108f7a9b0767c84841070744ff92
chi-hun <huunnii@gmail.com>
2025-07-22 08:31:22 +0000

    some

--- main.cpp
+++ main.cpp
@@ -1,63 +1,43 @@
-// 10-4
+// 12-1
 #include <iostream>
-#include <vector>
-#include <algorithm> 
 
 using namespace std;
 
-struct User {
-    string name;
-    int level;
-
-    User(string n, int l) : name(n), level(l) {}
-    bool operator==(const User& u) {
-        if (name == u.name && level == u.level) return true;
-        return false;
-    }
-};
-
-class Party {
+template <typename T>
+class Vector {
     private:
-        vector<User> users;
+        T* data;
+        size_t size_;
 
     public:
-        bool add_player(string name, int level) {
-            User new_user(name, level);
-            if (find(users.begin(), users.end(), new_user) != users.end()) {
-                return false;
+        Vector(size_t size) : size_(size) {
+            data = new T[size];
+            for (size_t i=0; i < size; i++) {
+                data[i] = 5;
             }
-            users.push_back(new_user);
-            return true;
+        }
+        ~Vector() {
+            delete[] data;
+        }
+        const T& at(size_t index) const {
+            if (index >= size_) {
+                throw out_of_range("띠디 안되요"); 
+            }
+            return data[index];
         }
 
-        bool able_dungeon (const int d_level) {
-            return all_of(users.begin(), users.end(), [d_level](User& user) -> bool {
-                return (user.level >= d_level);
-                }
-            );
-        }
-
-        bool use_item(const int i_level) {
-            return any_of(users.begin(), users.end(), [i_level](User& user) -> bool {
-                return (user.level >= i_level);
-                }
-            );
-        }
 };
 
+
 int main() {
-    Party party;
-    party.add_player("kim", 10);
-    party.add_player("lee", 20);
-    party.add_player("park", 25);
-    party.add_player("choi", 30);
-
-    cout << boolalpha;
-
-    cout << "20level dungeon : " << [&party](){if (party.able_dungeon(20)) return "able"; return "deable"; }() << endl;
-    cout << "10level dungeon : " << party.able_dungeon(10) << endl;
-    cout << "35level item : " << party.use_item(35) << endl;
-    cout << "30level item : " << party.use_item(30) << endl;
-
+    Vector<int> aa(10);
+    int data = 0;
+    try {
+        data = aa.at(12);
+    } catch (out_of_range& e) {
+        cout << "ㅋ" << e.what() << endl;
+    }
+    cout << data << endl;
     return 0;
 }
+
