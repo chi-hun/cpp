fd690c7bb1e663e5d95dc5bdf2865130c34da541
chi-hun <huunnii@gmail.com>
2025-07-23 08:05:56 +0000

    some

--- main.cpp
+++ main.cpp
@@ -1,43 +1,29 @@
-// 12-1
+// 13-1
 #include <iostream>
+#include <string>
+#include <utility>
 
 using namespace std;
 
-template <typename T>
-class Vector {
-    private:
-        T* data;
-        size_t size_;
-
+class A {
     public:
-        Vector(size_t size) : size_(size) {
-            data = new T[size];
-            for (size_t i=0; i < size; i++) {
-                data[i] = 5;
-            }
-        }
-        ~Vector() {
-            delete[] data;
-        }
-        const T& at(size_t index) const {
-            if (index >= size_) {
-                throw out_of_range("띠디 안되요"); 
-            }
-            return data[index];
-        }
+        A() {cout << "nomal create" << endl;}
+        A(const A& a) {cout << "copy create" << endl;}
+        A(const A&& a) {cout << "move create" << endl;}
+};
 
+
+class B {
+ public:
+  B(const A& a) : a_(std::move(a)) {}
+
+  A a_;
 };
 
 
 int main() {
-    Vector<int> aa(10);
-    int data = 0;
-    try {
-        data = aa.at(12);
-    } catch (out_of_range& e) {
-        cout << "ㅋ" << e.what() << endl;
-    }
-    cout << data << endl;
+    A a;
+    std::cout << "create B-- \n";
+    B b(move(a));
     return 0;
 }
-
