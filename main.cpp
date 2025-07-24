f487b9d5a9d28cadd9165593f8497886bf907e41
chi-hun <huunnii@gmail.com>
2025-07-24 08:40:38 +0000

    smart poiter

--- main.cpp
+++ main.cpp
@@ -1,29 +1,51 @@
-// 13-1
+// 13-2
 #include <iostream>
 #include <string>
 #include <utility>
+#include <memory>
+#include <vector>
+
 
 using namespace std;
 
 class A {
+    string s;
+    weak_ptr<A> other;
     public:
-        A() {cout << "nomal create" << endl;}
-        A(const A& a) {cout << "copy create" << endl;}
-        A(const A&& a) {cout << "move create" << endl;}
-};
-
-
-class B {
- public:
-  B(const A& a) : a_(std::move(a)) {}
-
-  A a_;
+        A(string s) : s(s) {
+            cout << "생성" << endl;
+        }
+        ~A() {
+            cout << "소멸" << endl;
+        }
+        void some() {
+            cout << "some" << endl;
+        }
+        void some_2() {
+            cout << "some_2" << endl;
+        }
+        void set_other(weak_ptr<A> o) {other = o;}
+        void acces_other() {
+            shared_ptr<A> o = other.lock();
+            if (o) {
+                cout << "name : "<< o -> name() << endl;
+            } else {
+                cout << "not other" << endl;
+            }
+        }
+        string name() {return s;}
 };
 
 
 int main() {
-    A a;
-    std::cout << "create B-- \n";
-    B b(move(a));
+    vector<shared_ptr<A>> v;
+    v.push_back(make_shared<A>("A"));
+    v.push_back(make_shared<A>("B"));
+    v[0] -> set_other(v[1]);
+    v[1] -> set_other(v[0]);
+    v[0] -> acces_other();
+    v.pop_back();
+    v[0] -> acces_other();
+
     return 0;
 }
