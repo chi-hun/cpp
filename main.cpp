9fae9940f65369ea9125bd85c39af03886b29ba3
chi-hun <huunnii@gmail.com>
2025-07-31 08:42:22 +0000

    make

--- main.cpp
+++ main.cpp
@@ -1,21 +1,9 @@
 // 17-1
-#include <iostream>
-using namespace std;
+#include <stdio.h>
 
-template<typename T>
-void what(T t) {
-    if constexpr (is_pointer<T>::value) {
-        cout << "포인트 : " << *t << endl;
-    } else {
-        cout << "포인트 아님 : " << t << endl;
-    }
-}
+// using namespace std;
 
 int main() {
-    int d = 3;
-    int* dp = &d;
-    what(d);
-    what(dp);
-
+    printf("Hello World!!\n");
     return 0;
 }
